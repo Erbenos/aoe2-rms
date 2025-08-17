6f10b3898141e9415e8233dba26ab5a7f3cee9ee
Miroslav Foltýn <miroslav.foltyn64@gmail.com>
2025-08-17 23:43:34 +0200

    fix the dependency graph - root lib should refer to doc lib too

--- aoe2-rms/info.rkt
+++ aoe2-rms/info.rkt
@@ -2,8 +2,8 @@
 
 (define license 'MIT)
 (define collection 'multi)
-(define deps '("aoe2-rms-lib" "aoe2-rms-test"))
-(define implies '("aoe2-rms-lib" "aoe2-rms-test"))
+(define deps '("aoe2-rms-lib" "aoe2-rms-test" "aoe2-rms-doc"))
+(define implies '("aoe2-rms-lib" "aoe2-rms-test" "aoe2-rms-doc"))
 (define pkg-desc "DSL for writing Age of Empires 2 Random Map Scripts.")
 (define pkg-authors '("Miroslav Foltýn"))
 (define version "0.1")
